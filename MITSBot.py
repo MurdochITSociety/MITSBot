19c377a52926bffe14723903f8020b922c24839c
Wolfleader <james.morgan@iinet.net.au>
2021-04-01 10:54:50 +0800

    added fact section

--- MITSBot.py
+++ MITSBot.py
@@ -35,18 +35,27 @@ helpEmbed = discord.Embed(
             )
 helpEmbed.set_author(name="MITSBot Commands", icon_url="https://cdn.discordapp.com/emojis/756060351854018610.png")
 helpEmbed.add_field(name='<:student:780271578608828437> __**Student Commands**__', value='\
-    `m!resources` Get study and support resources.', inline=False)
+    `m!resources` Get study and support resources. ', inline=False)
+    
 helpEmbed.add_field(name='_ _', value='_ _', inline=True) # Gap between command sections
 helpEmbed.add_field(name='<:birthday:779924273950490646> __**Birthday Commands**__', value=' \
     `m!addbday [Month] [Day]` Register a birthday. E.g. m!addbday January 1\n\
     `m!delbday` Remove a birthday.\n\
-    `m!viewbday` View today\'s birthdays.', inline=False)
+    `m!viewbday` View today\'s birthdays. ', inline=False)
+
+helpEmbed.add_field(name='_ _', value='_ _', inline=True) # Gap between command sections
+helpEmbed.add_field(name='<:book:> __**Fact Commands**__', value=' \
+    `m!catfact` Get a cool cat fact!\n\
+    ', inline=False)
+
 helpEmbed.add_field(name='_ _', value='_ _', inline=True) # Gap between command sections
 helpEmbed.add_field(name='<:dog:810097516409257984> __**Image Commands**__', value=' \
     `m!animal [animal]` Any animal you want (almost).\n\
     `m!dog` Pupper.\n\
     `m!cat` Cat.\n\
-    `m!harold` Harold.', inline=False)
+    `m!harold` Harold. ', inline=False)
+    
+    
 # Create Murdoch resources embed
 resourcesEmbed = discord.Embed(
                 title = '<:student:780271578608828437><:student:780271578608828437> Murdoch Resources <:student:780271578608828437><:student:780271578608828437>',
